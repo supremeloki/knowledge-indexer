807ea3da6f29cf1a8771eca548c8a1c4360a2eb5
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-11-14 21:06:48 +0330

    support config overrides pt2

--- src/knowledge_indexer/core.py
+++ src/knowledge_indexer/core.py
@@ -76,3 +76,5 @@ def chunk_document(doc: Document, max_words: int = DEFAULT_CHUNK_SIZE) -> list[I
             text=" ".join(window),
             token_count=len(window),
         ))
+    return chunks
+
